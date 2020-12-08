9f229abea82e378073c45ea2ceace2f9433f1bbd
Robert Roskam <raiderrobert@gmail.com>
2020-12-08 11:59:28 -0500

    more changes

--- python/tennis.py
+++ python/tennis.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 
+score_mapping = {
+    0 : "Love",
+    1 : "Fifteen",
+    2 : "Thirty",
+}
+
+
 class TennisGame1:
 
     def __init__(self, player1Name, player2Name):
@@ -16,14 +23,12 @@ class TennisGame1:
     
     def score(self):
         result = ""
-        tempScore=0
-        if (self.p1points==self.p2points):
-            result = {
-                0 : "Love-All",
-                1 : "Fifteen-All",
-                2 : "Thirty-All",
-            }.get(self.p1points, "Deuce")
-        elif (self.p1points>=4 or self.p2points>=4):
+        if self.p1points==self.p2points:
+            result = score_mapping.get(self.p1points)
+            result = result+'-All' if result is not None else 'Deuce' 
+        elif self.p1points<=3 and self.p2points<=3:
+            result += f'{score_mapping.get(self.p1points, "Forty")}-{score_mapping.get(self.p2points, "Forty")}'
+        elif self.p1points>=4 or self.p2points>=4:
             minusResult = self.p1points-self.p2points
             if (minusResult==1):
                 result ="Advantage " + self.player1Name
@@ -33,19 +38,6 @@ class TennisGame1:
                 result = "Win for " + self.player1Name
             else:
                 result ="Win for " + self.player2Name
-        else:
-            for i in range(1,3):
-                if (i==1):
-                    tempScore = self.p1points
-                else:
-                    result+="-"
-                    tempScore = self.p2points
-                result += {
-                    0 : "Love",
-                    1 : "Fifteen",
-                    2 : "Thirty",
-                    3 : "Forty",
-                }[tempScore]
         return result
 
 
